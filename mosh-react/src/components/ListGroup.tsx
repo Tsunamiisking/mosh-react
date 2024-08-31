f8d39143a48b547054702f08d265f53af25fb6b8
Allen Douglas <allenissabigboi@gmail.com>
2024-08-31 16:35:50 -0700

    Configured ListGroup Component

--- mosh-react/src/components/ListGroup.tsx
+++ mosh-react/src/components/ListGroup.tsx
@@ -1,17 +1,23 @@
 
 
 function ListGroup() {
+
+    const items = [
+        'New York',
+        'Italy',
+        'Japanese',
+        'France',
+        'Congo'
+    ]
     // Using "<> </>" as "fragments" to wrap all the elements in this component 
     // because we cannot render more than one element in a component that is not nested
     return (
         <> 
         <h1>List</h1>
         <ul className="list-group">
-        <li className="list-group-item">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Morbi leo risus</li>
-        <li className="list-group-item">Porta ac consectetur ac</li>
-        <li className="list-group-item">Vestibulum at eros</li>
+        {/* Using <list>.map to render the values in the list 
+        embeding them in an 'li' tag*/}
+        {items.map(item => <li>{item}</li>)}
     </ul>
         </>
     );
