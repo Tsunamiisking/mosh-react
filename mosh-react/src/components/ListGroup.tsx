1f42ed2a3c70cb0ade0d72ee3b7abf4d8293930e
Allen Douglas <allenissabigboi@gmail.com>
2024-08-31 17:34:06 -0700

    Added more functionality to ListGroup component

--- mosh-react/src/components/ListGroup.tsx
+++ mosh-react/src/components/ListGroup.tsx
@@ -1,23 +1,29 @@
 
 
 function ListGroup() {
-
-    const items = [
-        'New York',
-        'Italy',
-        'Japanese',
-        'France',
-        'Congo'
+    // List of of bjects
+    let items = [
+        {City: 'New York', Id: 1},
+        {City: 'Italy', Id: 2},
+        {City: 'Japanese', Id: 3},
+        {City: 'France', Id: 4},
+        {City: 'Congo', Id: 5}
     ]
+    // items = []
     // Using "<> </>" as "fragments" to wrap all the elements in this component 
     // because we cannot render more than one element in a component that is not nested
     return (
         <> 
         <h1>List</h1>
+        {/* Checks if items are inside the items list.. 
+        in logical operations, true && <value> returns <value>, 
+        false && <value> returns <value> */}
+        {items.length === 0 && <p>No list item</p>}
         <ul className="list-group">
         {/* Using <list>.map to render the values in the list 
         embeding them in an 'li' tag*/}
-        {items.map(item => <li>{item}</li>)}
+        {/* Indexing List objects for data usage */}
+        {items.map(item => <li className="list-group-item" key={item.Id} onDoubleClick={() => {console.log(item.City)}}>{item.City}</li>)}
     </ul>
         </>
     );
